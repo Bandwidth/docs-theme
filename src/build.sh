8dd33521c41edf3c0b558eda1ba17408e7f36d3a
Dan Tolbert <dtolbert@bandwidth.com>
2017-01-16 13:24:08 -0500

    update build script, add nvmrc to keep things stable

--- src/build.sh
+++ src/build.sh
@@ -11,3 +11,6 @@ uglifyjs -mc -- src/js/theme-api.js > assets/theme-api.js
 
 # Compile Website CSS
 lessc -clean-css src/less/website.less assets/theme-api.css
+
+# Copy over fonts
+cp -a src/less/website/fonts assets
